184ba969de25b5b979ff0cf6282220538bb08d2c
Rajiya Sultana <rssumu04@gmail.com>
2025-07-18 10:23:16 +0600

    Added custom input action for player

--- projectboost/player.gd
+++ projectboost/player.gd
@@ -3,13 +3,13 @@ extends RigidBody3D
 func _process(delta: float) -> void:
 	
 	var UpForce: float = 1000.0
-	var torque: Vector3 = Vector3(0.0, 0.0, 100.0 * delta)
+	var torque: Vector3 = Vector3(0.0, 0.0, 150.0 * delta)
 	
-	if Input.is_action_pressed("ui_accept"):
-		apply_central_force(Vector3.UP * delta * UpForce)
+	if Input.is_action_pressed("boost"):
+		apply_central_force(basis.y * delta * UpForce)
 		
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("rotate_right"):
 		apply_torque(-torque)
 		
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("rotate_left"):
 		apply_torque(torque)
