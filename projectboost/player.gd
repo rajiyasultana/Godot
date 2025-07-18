cc1b5112109d31b8d07816631408858fce65505f
Rajiya Sultana <rssumu04@gmail.com>
2025-07-18 11:10:47 +0600

    Update player.gd

--- projectboost/player.gd
+++ projectboost/player.gd
@@ -1,15 +1,14 @@
 extends RigidBody3D
 
+@export_range(750, 3000) var UpForce: float = 1000.0
+@export var torqueForce: float = 150.0
+
 func _process(delta: float) -> void:
-	
-	var UpForce: float = 1000.0
-	var torque: Vector3 = Vector3(0.0, 0.0, 150.0 * delta)
-	
 	if Input.is_action_pressed("boost"):
 		apply_central_force(basis.y * delta * UpForce)
 		
 	if Input.is_action_pressed("rotate_right"):
-		apply_torque(-torque)
+		apply_torque(Vector3(0.0, 0.0, -torqueForce * delta))
 		
 	if Input.is_action_pressed("rotate_left"):
-		apply_torque(torque)
+		apply_torque(Vector3(0.0, 0.0, torqueForce * delta))
