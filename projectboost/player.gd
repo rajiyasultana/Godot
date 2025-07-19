88344bef3077a9d59b8300707a2e4b52e56c0674
Rajiya Sultana <rssumu04@gmail.com>
2025-07-19 07:25:20 +0600

    Added new level

--- projectboost/player.gd
+++ projectboost/player.gd
@@ -12,3 +12,18 @@ func _process(delta: float) -> void:
 		
 	if Input.is_action_pressed("rotate_left"):
 		apply_torque(Vector3(0.0, 0.0, torqueForce * delta))
+
+func _on_body_entered(body: Node) -> void:
+	if "Goal" in body.get_groups():
+		completeLevel(body.file_path)
+
+	if "Hazard" in body.get_groups():
+		crushSequence() 
+
+func crushSequence() -> void:
+	print("BOOM!")
+	get_tree().reload_current_scene()
+	
+func completeLevel(nextLevelFile: String) -> void:
+	print("Complete!")
+	get_tree().change_scene_to_file(nextLevelFile)
