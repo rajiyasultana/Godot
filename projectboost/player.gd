9470eca0a4ce3e082f078d40bb208f8049fd2c42
Rajiya Sultana <rssumu04@gmail.com>
2025-07-19 10:12:36 +0600

    Update player.gd
    Added tween delay to change level

--- projectboost/player.gd
+++ projectboost/player.gd
@@ -33,4 +33,7 @@ func crushSequence() -> void:
 	
 func completeLevel(nextLevelFile: String) -> void:
 	print("Complete!")
-	get_tree().change_scene_to_file(nextLevelFile)
+	var tween = create_tween()
+	tween.tween_interval(1.0)
+	tween.tween_callback(get_tree().change_scene_to_file.bind(nextLevelFile))
+	
