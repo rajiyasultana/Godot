9631cd3333ea9fffde9bb33f2940434604c7b2f2
Rajiya Sultana <rssumu04@gmail.com>
2025-07-19 12:09:43 +0600

    Added audio

--- projectboost/player.gd
+++ projectboost/player.gd
@@ -2,12 +2,21 @@ extends RigidBody3D
 
 @export_range(750, 3000) var UpForce: float = 1000.0
 @export var torqueForce: float = 150.0
+
 var is_transitioning = false
+
+@onready var exploision_sound: AudioStreamPlayer = $"Exploision sound"
+@onready var success_sound: AudioStreamPlayer = $"Success Sound"
+@onready var rocket_audio: AudioStreamPlayer3D = $RocketAudio
 
 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
 		apply_central_force(basis.y * delta * UpForce)
+		if rocket_audio.playing == false:
+			rocket_audio.play()
+	else:
+		rocket_audio.stop()
 		
 	if Input.is_action_pressed("rotate_right"):
 		apply_torque(Vector3(0.0, 0.0, -torqueForce * delta))
@@ -25,15 +34,18 @@ func _on_body_entered(body: Node) -> void:
 
 func crushSequence() -> void:
 	print("BOOM!")
+	exploision_sound.play()
 	set_process(false)
 	is_transitioning = true
 	var tween = create_tween()
-	tween.tween_interval(1.0)
+	tween.tween_interval(2.5)
 	tween.tween_callback(get_tree().reload_current_scene)
 	
 func completeLevel(nextLevelFile: String) -> void:
 	print("Complete!")
+	success_sound.play()
+	set_process(false)
 	var tween = create_tween()
-	tween.tween_interval(1.0)
+	tween.tween_interval(2)
 	tween.tween_callback(get_tree().change_scene_to_file.bind(nextLevelFile))
 	
