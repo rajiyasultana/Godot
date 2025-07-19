8b64a27f746ebd751f6a435a327d9bff469ac2d1
Rajiya Sultana <rssumu04@gmail.com>
2025-07-19 11:25:35 +0600

    Added tween for moving object

---
+++ projectboost/moving_hazard.gd
@@ -0,0 +1,11 @@
+extends AnimatableBody3D
+
+@export var destination: Vector3
+@export var  duration: float = 1.0
+
+func _ready() -> void:
+	var tween = create_tween()
+	tween.set_loops()
+	tween.set_trans(Tween.TRANS_SINE)
+	tween.tween_property(self, "global_position", global_position + destination, duration)
+	tween.tween_property(self, "global_position", global_position, duration)
