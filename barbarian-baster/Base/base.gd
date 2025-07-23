9ff118e0f15dcc9d06b17db8c6741d5167bde853
Rajiya Sultana <rssumu04@gmail.com>
2025-07-24 01:32:41 +0600

    Update base.gd
    Using lerp function to change the health color

--- barbarian-baster/Base/base.gd
+++ barbarian-baster/Base/base.gd
@@ -4,7 +4,10 @@ extends Node3D
 var current_health: int:
 	set(health_in):
 		current_health = health_in
-		label_3d.text = str(current_health)
+		label_3d.text = str(current_health) + "/" + str(max_health)
+		var red: Color = Color.RED
+		var white: Color = Color.WHITE
+		label_3d.modulate = red.lerp(white, float(current_health) / float(max_health))
 		if current_health < 1:
 			get_tree().reload_current_scene()
 		
