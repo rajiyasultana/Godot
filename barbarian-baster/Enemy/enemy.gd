0c3542df6f190c79f9c3326ae6ed46cfa44303a1
Rajiya Sultana <rssumu04@gmail.com>
2025-07-28 03:00:49 +0600

    Added earning and spending gold system

--- barbarian-baster/Enemy/enemy.gd
+++ barbarian-baster/Enemy/enemy.gd
@@ -2,16 +2,20 @@ extends PathFollow3D
 
 @export var speed: float = 2.5
 @export var max_health: int = 50;
+@export var gold_value := 15
 var current_health: int:
 	set(health_in):
 		if health_in < current_health:
 			animation_player.play("TakeDamage")
 		current_health = health_in
 		if current_health < 1:
+			bank.gold += gold_value
 			queue_free()
+
 
 @onready var base = get_tree().get_first_node_in_group("base")
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var bank = get_tree().get_first_node_in_group("bank")
 
 func _ready() -> void:
 	current_health = max_health
--- barbarian-baster/RayPIckerCamera/ray_picker_camera.gd
+++ barbarian-baster/RayPIckerCamera/ray_picker_camera.gd
@@ -2,8 +2,10 @@ extends Camera3D
 
 @export var grid_map: GridMap
 @export var turret_manager: Node3D
+@export var turret_cost := 100
 
 @onready var ray_cast_3d: RayCast3D = $RayCast3D
+@onready var bank = get_tree(). get_first_node_in_group("bank")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,16 +15,20 @@ func _process(delta: float) -> void:
 	ray_cast_3d.force_raycast_update()
 	
 	if ray_cast_3d.is_colliding():
-		Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
-		var collider = ray_cast_3d.get_collider()
-		if collider is GridMap: 
-			if Input.is_action_pressed("click"):
-				var collision_point = ray_cast_3d.get_collision_point()
-				var cell = grid_map.local_to_map(collision_point)
-				if grid_map.get_cell_item(cell) == 0:
-					grid_map.set_cell_item(cell, 1)
-					var turret_position = grid_map.map_to_local(cell)
-					turret_manager.build_turret(turret_position)
+		if bank.gold >= turret_cost:
+			Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
+			var collider = ray_cast_3d.get_collider()
+			if collider is GridMap: 
+				if Input.is_action_pressed("click"):
+					var collision_point = ray_cast_3d.get_collision_point()
+					var cell = grid_map.local_to_map(collision_point)
+					if grid_map.get_cell_item(cell) == 0:
+						grid_map.set_cell_item(cell, 1)
+						var turret_position = grid_map.map_to_local(cell)
+						turret_manager.build_turret(turret_position)
+						bank.gold -= turret_cost
+		else:
+			Input.set_default_cursor_shape(Input.CURSOR_ARROW)
 			
 	else:
 		Input.set_default_cursor_shape(Input.CURSOR_ARROW)
