1240a95493e9b8b2815a9b9d6ab3bf335fde76a6
Rajiya Sultana <rssumu04@gmail.com>
2025-07-27 22:35:24 +0600

    Added timer

--- barbarian-baster/Turret/projectile.gd
+++ barbarian-baster/Turret/projectile.gd
@@ -5,3 +5,7 @@ var direction:= Vector3.FORWARD
 
 func _physics_process(delta: float) -> void:
 	position += direction * speed * delta
+
+
+func _on_timer_timeout() -> void:
+	queue_free()
--- barbarian-baster/Turret/turret.gd
+++ barbarian-baster/Turret/turret.gd
@@ -1,7 +1,9 @@
 extends Node3D
 
 @export var projectile: PackedScene
+@onready var turret_top: MeshInstance3D = $TurretBase/TurretTop
 
-func _ready() -> void:
+func _on_timer_timeout() -> void:
 	var shot = projectile.instantiate()
 	add_child(shot)
+	shot.global_position = turret_top.global_position
