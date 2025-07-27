f6490d4f6485f417edcaf2eb689a57fd5b9ef76c
Rajiya Sultana <rssumu04@gmail.com>
2025-07-28 00:25:53 +0600

    Destroying enemies.

--- barbarian-baster/Enemy/enemy.gd
+++ barbarian-baster/Enemy/enemy.gd
@@ -1,9 +1,18 @@
 extends PathFollow3D
 
 @export var speed: float = 2.5
+@export var max_health: int = 50;
+var current_health: int:
+	set(health_in):
+		current_health = health_in
+		if current_health < 1:
+			queue_free()
 
 @onready var base = get_tree().get_first_node_in_group("base")
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+func _ready() -> void:
+	current_health = max_health
+
 func _process(delta: float) -> void:
 	progress += delta *speed
 	if(progress_ratio == 1):
